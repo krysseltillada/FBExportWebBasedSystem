cb551d068979c2185a3fb12ca0c92425e3977d69
Kryssel Tillada <krysseltillada@gmail.com>
2017-12-19 03:15:36 +0800

    created a workspace for the project

---
+++ FBExportWebBasedSystem/FBExportOrderSystem/src/main/java/com/fb/exportorder/FbExportOrderSystemApplication.java
@@ -0,0 +1,12 @@
+package com.fb.exportorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbExportOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbExportOrderSystemApplication.class, args);
+	}
+}
---
+++ FBExportWebBasedSystem/FBExportOrderSystem/src/test/java/com/fb/exportorder/FbExportOrderSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fb.exportorder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FbExportOrderSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
