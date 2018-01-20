2078258ee355d44562fac53b647564fbb1801fb2
Kryssel Tillada <krysseltillada@gmail.com>
2018-01-20 23:40:02 +0800

    redesigned dashboard interface in admin site

--- FBExportWebBasedSystem/FBExportOrderSystem/src/main/webapp/WEB-INF/views/admin/admin-ui-template/js/charts-home.js
+++ FBExportWebBasedSystem/FBExportOrderSystem/src/main/webapp/WEB-INF/views/admin/admin-ui-template/js/charts-home.js
@@ -41,7 +41,7 @@ $(document).ready(function () {
             labels: ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sept", "oct", "nov", "dec"],
             datasets: [
                 {
-                    label: "Pending orders",
+                    label: "Paid orders",
                     fill: true,
                     lineTension: 0,
                     backgroundColor: "transparent",
@@ -112,6 +112,101 @@ $(document).ready(function () {
             ]
         }
     });
+
+    // ------------------------------------------------------- //
+    // Charts Gradients
+    // ------------------------------------------------------ //
+    var ctx1 = $("canvas").get(0).getContext("2d");
+    var gradient1 = ctx1.createLinearGradient(150, 0, 150, 300);
+    gradient1.addColorStop(0, 'rgba(133, 180, 242, 0.91)');
+    gradient1.addColorStop(1, 'rgba(255, 119, 119, 0.94)');
+
+    var gradient2 = ctx1.createLinearGradient(146.000, 0.000, 154.000, 300.000);
+    gradient2.addColorStop(0, 'rgba(104, 179, 112, 0.85)');
+    gradient2.addColorStop(1, 'rgba(76, 162, 205, 0.85)');
+
+
+    // ------------------------------------------------------- //
+    // Line Chart
+    // ------------------------------------------------------ //
+    var LINECHARTEXMPLE   = $('#lineChartExample');
+    var lineChartExample = new Chart(LINECHARTEXMPLE, {
+        type: 'line',
+        options: {
+            title : {
+                display: true,
+                text : "Montly page status"
+            },
+            
+            legend: {labels:{fontColor:"#777", fontSize: 12}},
+            scales: {
+                xAxes: [{
+                    display: true,
+                    gridLines: {
+                        color: '#eee'
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    gridLines: {
+                        color: '#eee'
+                    }
+                }]
+            },
+        },
+        data: {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    label: "Visitors",
+                    fill: true,
+                    lineTension: 0.3,
+                    backgroundColor: gradient1,
+                    borderColor: gradient1,
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    borderWidth: 1,
+                    pointBorderColor: gradient1,
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: gradient1,
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [30, 50, 40, 61, 42, 35, 40],
+                    spanGaps: false
+                },
+                {
+                    label: "Page views",
+                    fill: true,
+                    lineTension: 0.3,
+                    backgroundColor: gradient2,
+                    borderColor: gradient2,
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    borderWidth: 1,
+                    pointBorderColor: gradient2,
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: gradient2,
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [50, 40, 50, 40, 45, 40, 30],
+                    spanGaps: false
+                }
+            ]
+        }
+    });
+
 
 
 
