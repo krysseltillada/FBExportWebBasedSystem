7ad037ec86fd68e23998415ed9a55b6faaf56fe5
Kryssel Tillada <krysseltillada@gmail.com>
2018-01-25 03:49:15 +0800

    designed add new product page (continuing later the multiple upload images)

--- FBExportWebBasedSystem/FBExportOrderSystem/src/main/webapp/WEB-INF/views/admin/admin-ui-template/js/inventory.js
+++ FBExportWebBasedSystem/FBExportOrderSystem/src/main/webapp/WEB-INF/views/admin/admin-ui-template/js/inventory.js
@@ -8,37 +8,39 @@ $(document).on('change', '.btn-file :file', function() {
             label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
             
 		input.trigger('fileselect', [label]);
-		});
+});
 
-		$('.btn-file :file').on('fileselect', function(event, label) {
-		    
-		    var input = $(this).parents('.input-group').find(':text'),
-		        log = label;
-		    
-		    if( input.length ) {
-		        input.val(log);
-		    } else {
-		        if( log ) alert(log);
-		    }
-	    
-		});
-		function readURL(input) {
-
-		    if (input.files && input.files[0] && allowedImageTypes.includes(input.files[0].type)) {
-                var reader = new FileReader();
+$('.btn-file :file').on('fileselect', function(event, label) {
+    
+    var input = $(this).parents('.input-group').find(':text'),
+        log = label;
                 
-		        
-		        reader.onload = function (e) {  
-		            $('#img-upload').attr('src', e.target.result);
-		        }
-		        
-		        reader.readAsDataURL(input.files[0]);
-		    } else {
-                window.alert("invalid image type");
-                $("input[type=text]").val("");
-            }
-		}
+    if( input.length && allowedImageTypes.includes(event.target.files[0].type)) {
+        input.val(log);
+    } else {
+        if( log ) {
+            alert("invalid image type");
+            input.val("");
+        }
+    }
 
-		$("#imgInp").change(function(){
-		    readURL(this);
-        }); 
+});
+
+function readURL(input) {
+
+    if (input.files && input.files[0] && allowedImageTypes.includes(input.files[0].type)) {
+        var reader = new FileReader();
+        
+        reader.onload = function (e) {  
+            $('#img-upload').attr('src', e.target.result);
+        }
+        
+        reader.readAsDataURL(input.files[0]);
+    } else {
+        $("#img-upload").attr('src', "upload-product-img.png");
+    }
+}
+
+$("#imgInp").change(function(){
+    readURL(this);
+}); 
