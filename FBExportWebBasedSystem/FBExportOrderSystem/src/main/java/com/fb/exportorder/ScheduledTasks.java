b959a607990bd66293e135475141c20a6f4220a5
Alfred <alfredoborja81194@gmail.com>
2018-04-03 13:56:25 +0800

    save some works

--- FBExportWebBasedSystem/FBExportOrderSystem/src/main/java/com/fb/exportorder/ScheduledTasks.java
+++ FBExportWebBasedSystem/FBExportOrderSystem/src/main/java/com/fb/exportorder/ScheduledTasks.java
@@ -5,6 +5,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
